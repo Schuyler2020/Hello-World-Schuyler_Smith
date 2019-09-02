4b522e2dee8903ce227b3d3f3630a1791ef9b63f
Schuyler Smith <ssmith969573@knights.ucf.edu>
2019-09-01 22:56:22 -0400

    First Commit

---
+++ Smith_HelloWorld/src/Smith_hello.java
@@ -0,0 +1,15 @@
+
+public class Smith_hello {
+
+	 public void message()
+	    {
+	        System.out.println("Hello World.");
+	    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Smith_hello obj=new Smith_hello();
+	        obj.message();
+	}
+
+}
